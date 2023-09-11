0e5f5aff8be5caca0a02c29fd0b2606b0192c7f0
AntixxTechHub <info@antixxtechhub.com>
2023-09-11 18:55:00 +0530

    new update with server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  // url: env('PUBLIC_URL', 'https://dealdoxstrapi.pbwebvision.com/'),
+  url: env('PUBLIC_URL', 'https://cms.dealdox.io'),
   app: {
     keys: env.array('APP_KEYS'),
   },
