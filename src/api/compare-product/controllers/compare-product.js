3c8663a3f40ccd92842e501539589099f9a9a6fc
Zahira-SPM-DEALDOCX <zahira.ln@spmglobaltech.com>
2024-09-05 19:15:48 +0530

    created product comparision page

---
+++ src/api/compare-product/controllers/compare-product.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * compare-product controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::compare-product.compare-product');
---
+++ src/api/compare-product/routes/compare-product.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * compare-product router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::compare-product.compare-product');
