c7db6c590206ff4445f786f6bb11120faee3fa6a
Zahira-SPM-DEALDOCX <zahira.ln@spmglobaltech.com>
2024-12-16 11:13:28 +0530

    updated old middleware code

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,32 +1,3 @@
-// module.exports = [
-//   'strapi::errors',
-//   {
-//     name: 'strapi::security',
-//     config: {
-//       contentSecurityPolicy: {
-//         useDefaults: true,
-//         directives: {
-//           "connect-src": ["'self'", "https:"],
-//           "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-//           "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-//           upgradeInsecureRequests: null,
-//         },
-//       },
-//     },
-//   },
-//   // 'strapi::security',
-//   'strapi::cors',
-//   'strapi::poweredBy',
-//   'strapi::logger',
-//   'strapi::query',
-//   'strapi::body',
-//   'strapi::session',
-//   'strapi::favicon',
-//   'strapi::public',
-// ];
-
-
-
 module.exports = [
   'strapi::errors',
   {
@@ -43,15 +14,8 @@ module.exports = [
       },
     },
   },
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['https://www.dealdox.io'], // Replace with your frontend URL or use '*' for all origins
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-      headers: ['Content-Type', 'Authorization'],
-      keepHeaderOnError: true,
-    },
-  },
+  // 'strapi::security',
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
@@ -60,4 +24,40 @@ module.exports = [
   'strapi::favicon',
   'strapi::public',
 ];
+
+
+
+// module.exports = [
+//   'strapi::errors',
+//   {
+//     name: 'strapi::security',
+//     config: {
+//       contentSecurityPolicy: {
+//         useDefaults: true,
+//         directives: {
+//           "connect-src": ["'self'", "https:"],
+//           "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+//           "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+//           upgradeInsecureRequests: null,
+//         },
+//       },
+//     },
+//   },
+//   {
+//     name: 'strapi::cors',
+//     config: {
+//       origin: ['https://www.dealdox.io'], // Replace with your frontend URL or use '*' for all origins
+//       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+//       headers: ['Content-Type', 'Authorization'],
+//       keepHeaderOnError: true,
+//     },
+//   },
+//   'strapi::poweredBy',
+//   'strapi::logger',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
 
