0f3510e7e7025e8b6d9cb31d6b14c16d8227889c
Zahira-SPM-DEALDOCX <zahira.ln@spmglobaltech.com>
2025-01-16 13:54:50 +0530

    updated code

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://cms.dealdox.io'),
+  // url: env('PUBLIC_URL', 'https://cms.dealdox.io'),
 
 
   app: {
